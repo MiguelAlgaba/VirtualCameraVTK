50da1f866431041dada0cc17c023b3a451cb81ee
Miguel Algaba <miguel.algaba.borrego@gmail.com>
2012-11-26 15:15:21 +0100

    Now the camera pose and focal length are specified through the program arguments

--- apps/vtkCamera/Camera.cxx
+++ apps/vtkCamera/Camera.cxx
@@ -17,21 +17,36 @@
 #include <vtkAxesActor.h>
 #include <string>
 #include <ostream>
+#include <sstream>
  
 int main(int argc, char *argv[])
 {
   // Parse command line arguments
-  if(argc != 2)
+  if(argc != 9)
     {
-    std::cout << "Usage: " << argv[0] << " Filename(.obj)" << std::endl;
+    std::cout << "Usage: " << argv[0] << " Filename(.obj) yaw pitch roll (camera rotation [rad]) tx ty tz (camera translation [cm]) f (focal length [cm] > 0)" << std::endl;
     return EXIT_FAILURE;
     }
- 
+
   std::string filename = argv[1];
   vtkSmartPointer<vtkOBJReader> reader =
     vtkSmartPointer<vtkOBJReader>::New();
   reader->SetFileName(filename.c_str());
   reader->Update();
+
+  std::istringstream ss;  
+  double yaw,pitch,roll,tx,ty,tz,focalLength;
+  ss.clear(); ss.str(argv[2]); ss >> yaw;         //rad
+  ss.clear(); ss.str(argv[3]); ss >> pitch;       //rad
+  ss.clear(); ss.str(argv[4]); ss >> roll;        //rad
+  ss.clear(); ss.str(argv[5]); ss >> tx;          //cm
+  ss.clear(); ss.str(argv[6]); ss >> ty;          //cm
+  ss.clear(); ss.str(argv[7]); ss >> tz;          //cm
+  ss.clear(); ss.str(argv[8]); ss >> focalLength; //cm
+
+  std::cout << "camera extrinsic parameters: (" 
+            << yaw << "," << pitch << "," << roll << ","
+            << tx << "," << ty << "," << tz << ")" << std::endl; 
  
   // Create a mapper and actor
   vtkSmartPointer<vtkPolyDataMapper> mapper = 
@@ -42,24 +57,18 @@ int main(int argc, char *argv[])
     vtkSmartPointer<vtkActor>::New();
   actor->SetMapper(mapper);
 
-  // Define the 3 camera poses (front view, right view, left view)
-  const double cameraPose0[]={-1.0,0.0,0.0,0.0,
-                              0.0,1.0,0.0,0.0,
-                              0.0,0.0,-1.0,-200.0,
-                              0.0,0.0,0.0,1.0};
-  const double cameraPose1[]={0.0,0.0,-1.0,-200.0,
-                              0.0,1.0,0.0,0.0,
-                              1.0,0.0,0.0,0.0,
-                              0.0,0.0,0.0,1.0};
-  const double cameraPose2[]={0.0,0.0,1.0,200.0,
-                              0.0,1.0,0.0,0.0,
-                             -1.0,0.0,0.0,0.0,
-                              0.0,0.0,0.0,1.0};
+  // Define the camera pose
+  double cy,cp,cr,sy,sp,sr;
+  cy=cos(yaw); cp=cos(pitch); sy=sin(yaw);
+  sp=sin(pitch);sr=sin(roll); cr=cos(roll);
+  const double cameraPoseMatrix[]={cy*cp , cy*sp*sr-sy*cr , cy*sp*cr+sy*sr , tx ,
+                                   sy*cp , sy*sp*sr+cy*cr , sy*sp*cr-cy*sr , ty ,
+                                   -sp   , cp*sr          , cp*cr          , tz ,
+                                   0.0   , 0.0            , 0.0            , 1.0};
   vtkSmartPointer<vtkTransform> cameraPose = 
     vtkSmartPointer<vtkTransform>::New();
-  cameraPose->SetMatrix(cameraPose0);
+  cameraPose->SetMatrix(cameraPoseMatrix);
 
-  double focalLength = 5; //in cm
   vtkSmartPointer<vtkCamera> camera = 
     vtkSmartPointer<vtkCamera>::New();
   camera->SetPosition(0,0,focalLength);
